8e1dd14422c4ce2ea42bde28bab56aab13b0ffda
jonathan <jon@thanlocke.com>
2021-06-09 06:26:00 -0700

    🚧 Fix build

--- kivakit-aws/s3/src/main/java/module-info.java
+++ kivakit-aws/s3/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module kivakit.aws.s3
     requires transitive kivakit.aws.core;
     requires transitive kivakit.resource;
 
+    requires transitive software.amazon.awssdk.http;
     requires transitive software.amazon.awssdk.services.s3;
     requires transitive software.amazon.awssdk.core;
     requires transitive software.amazon.awssdk.regions;
