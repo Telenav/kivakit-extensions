283e7fe53d11e99816acbac78e63dbb635a030b3
jonathan <jon@thanlocke.com>
2021-09-03 20:53:19 -0700

    👷 Add build feature branch workflow

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -13,18 +13,17 @@ BRANCH="${GITHUB_REF//refs\/heads\//}:"
 echo "Building feature branch $BRANCH"
 
 echo "Cloning kivakit-extensions in $ROOT"
+cd "$ROOT"
+git clone --branch "$BRANCH" --quiet https://github.com/Telenav/kivakit.git
 
-git clone --quiet https://github.com/Telenav/kivakit.git
-cd "$ROOT"/kivakit
-git checkout "$BRANCH"
-cd superpom
+echo "Installing kivakit super POM"
+cd "$ROOT"/kivakit/superpom
 mvn clean install
 
 echo "Cloning kivakit-extensions in $ROOT"
+cd "$ROOT"
+git clone --branch "$BRANCH"--quiet https://github.com/Telenav/kivakit-extensions.git
 
-git clone --quiet https://github.com/Telenav/kivakit-extensions.git
-cd "$ROOT"/kivakit-extensions
-git checkout "$BRANCH"
-
+echo "Building kivakit-extensions"
 cd "$ROOT"/kivakit-extensions
 mvn -Dmaven.javadoc.skip=true -DKIVAKIT_DEBUG="!Debug" -P shade -P tools --no-transfer-progress --batch-mode clean install
