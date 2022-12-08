522eea6c81e362c8045bb80d7806092d180ed828
jonathanl-telenav <jonathanl@telenav.com>
2022-12-08 14:07:19 -0700

    ✨ Added project and build properties per application/project

--- kivakit-microservice/src/main/java/com/telenav/kivakit/microservice/MicroserviceProject.java
+++ kivakit-microservice/src/main/java/com/telenav/kivakit/microservice/MicroserviceProject.java
@@ -1,13 +1,14 @@
 package com.telenav.kivakit.microservice;
 
 import com.telenav.kivakit.annotations.code.quality.CodeQuality;
+import com.telenav.kivakit.core.BaseKivaKitProject;
 import com.telenav.kivakit.core.collections.set.ObjectSet;
 import com.telenav.kivakit.core.project.Project;
 import com.telenav.kivakit.core.project.ProjectTrait;
 import com.telenav.kivakit.settings.SettingsProject;
 
-import static com.telenav.kivakit.annotations.code.quality.Stability.STABLE_EXTENSIBLE;
 import static com.telenav.kivakit.annotations.code.quality.Documentation.DOCUMENTATION_COMPLETE;
+import static com.telenav.kivakit.annotations.code.quality.Stability.STABLE_EXTENSIBLE;
 import static com.telenav.kivakit.annotations.code.quality.Testing.UNTESTED;
 import static com.telenav.kivakit.core.collections.set.ObjectSet.set;
 
@@ -21,7 +22,7 @@ import static com.telenav.kivakit.core.collections.set.ObjectSet.set;
 @CodeQuality(stability = STABLE_EXTENSIBLE,
              testing = UNTESTED,
              documentation = DOCUMENTATION_COMPLETE)
-public class MicroserviceProject extends Project
+public class MicroserviceProject extends BaseKivaKitProject
 {
     @Override
     public ObjectSet<Class<? extends Project>> dependencies()
